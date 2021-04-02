6e2535d88019717f871772d74e8d942e57e448f9
Deeriv11 <deeriv11@gmail.com>
2021-04-02 11:34:59 -0400

    2 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,305 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   template: `
     <div class="navbar is-dark">
-    <div class="navbar-brand">
+      <div class="navbar-brand">
+        <a href="#" class="navbar-item">
+          <figure class="image is-9by1">
+            <img src="https://et710.com/website/war2/img/navbar-logo.jpg" />
+          </figure>
+        </a>
+      </div>
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
 
-     <a href="#" class="navbar-item">
-     <figure class="image is-9by1">
-      <img src="../assets/img/img/navbar-logo.jpg">
-      </figure>
-      </a>
+      <div class="dropdown is-active">
+        <div class="dropdown-trigger is-active">
+          <button
+            class="button"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu3"
+          >
+            <span>MENU</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+        <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+          <div class="dropdown-content">
+            <a href="#" class="dropdown-item">
+              HOME
+            </a>
 
-     </div>
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;   
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;    
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;    
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;    
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;    
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;    
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      &nbsp;
-      
- <div class="dropdown is-active">
-  <div class="dropdown-trigger is-active">
-    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
-      <span>MENU</span>
-      <span class="icon is-small">
-        <i class="fas fa-angle-down" aria-hidden="true"></i>
-      </span>
-    </button>
-  </div>
-  <div class="dropdown-menu" id="dropdown-menu3" role="menu">
-    <div class="dropdown-content">
-      <a href="#" class="dropdown-item">
-       HOME
-      </a>
-      
-      <a class="dropdown-item" routerLink="/graphic-design">
-        Graphic Design
-      </a>
-      <a class="dropdown-item" routerLink ="/InDesign">
-        InDesign
-      </a>
-      <a  class="dropdown-item" routerLink="/DigitalArt">
-        Digitial Art
-      </a>
-      <a class="dropdown-item" routerLink="/Illustrations">
-        Illustrations
-      </a>
-      <a  class="dropdown-item" routerLink="/Paintings">
-      Paintings
-      </a>
-     <a  class="dropdown-item" routerLink="/AboutMe">
-        ABOUT ME
-      </a>
+            <a class="dropdown-item" routerLink="/graphic-design">
+              Graphic Design
+            </a>
+            <a class="dropdown-item" routerLink="/InDesign">
+              InDesign
+            </a>
+            <a class="dropdown-item" routerLink="/DigitalArt">
+              Digitial Art
+            </a>
+            <a class="dropdown-item" routerLink="/Illustrations">
+              Illustrations
+            </a>
+            <a class="dropdown-item" routerLink="/Paintings">
+              Paintings
+            </a>
+            <a class="dropdown-item" routerLink="/AboutMe">
+              ABOUT ME
+            </a>
 
-      <a  class="dropdown-item" routerLink="/ContactMe">
-        CONTACT ME 
-      </a>
+            <a class="dropdown-item" routerLink="/ContactMe">
+              CONTACT ME
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,63 +1,122 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   template: `
-     <img src ="../assets/img/img/sudeoart.jpg">
+    <img src="https://et710.com/website/war2/img/loona-black.jpg"/>
 
-    <div class="multilevel-offcanvas off-canvas position-right" id="offCanvasRight" data-off-canvas>
+    <div
+      class="multilevel-offcanvas off-canvas position-right"
+      id="offCanvasRight"
+      data-off-canvas
+    >
+      <ul class="menu vertical nested">
+        <div class="columns">
+          <div class="column">
+            <li>
+              <img
+                src="https://et710.com/website/war2/img/Wolf-Kings.jpg"
+                class="image is-16by9"
+              /><button
+                class="button is-hoverable is-dark"
+                routerLink="/graphic-design"
+              >
+                Graphic Design
+              </button>
+            </li>
+          </div>
 
-            <ul class="menu vertical nested">
-             <div class="columns">
-    
-              <div class="column">
-              <li><img src="https://et710.com/website/war2/img/Wolf-Kings.jpg" class="image is-16by9"><button class="button is-hoverable is-dark" routerLink="/graphic-design">Graphic Design</button></li>
-              </div>
+          <div class="column">
+            <li>
+              <img
+                src="https://et710.com/website/war2/img/stargazer.png"
+                class="image is-16by9"
+              /><button
+                class="button is-hoverable is-dark"
+                routerLink="/DigitalArt"
+              >
+                Digital Art
+              </button>
+            </li>
+          </div>
 
-              <div class="column">
-              <li><img src="https://et710.com/website/war2/img/stargazer.png" class="image is-16by9"><button class="button is-hoverable is-dark" routerLink="/DigitalArt">Digital Art</button></li>
-             </div>
+          <div class="column">
+            <li>
+              <img
+                src="https://et710.com/website/war2/img/shinee-moon.jpg"
+                class="image is-16by9"
+              /><button
+                class="button is-hoverable is-dark"
+                routerLink="/Illustrations"
+              >
+                Illustrations
+              </button>
+            </li>
+          </div>
 
-               <div class="column">
-              <li><img src="https://et710.com/website/war2/img/shinee-moon.jpg" class="image is-16by9"><button class="button is-hoverable is-dark" routerLink="/Illustrations">Illustrations</button></li>
-              </div> 
-              
-              <div class="column">
-              <li><img  src="https://et710.com/website/war2/img/exo-studios.jpg" class="image is-16by9"><button class="button is-hoverable is-dark" routerLink ="/InDesign">InDesign</button></li>
-             </div>
+          <div class="column">
+            <li>
+              <img
+                src="https://et710.com/website/war2/img/exo-studios.jpg"
+                class="image is-16by9"
+              /><button
+                class="button is-hoverable is-dark"
+                routerLink="/InDesign"
+              >
+                InDesign
+              </button>
+            </li>
+          </div>
 
-              <div class="column">
-              <li><<img src="https://et710.com/website/war2/img/Visionaire.jpg" class="image is-16by9"><button class="button is-hoverable is-dark" routerLink="/Paintings">Paintings</button></li>
-              </div>
-               
-              <div class="column">
-              <li><img src="https://et710.com/website/war2/img/name-logo.jpg" class="image is-16by9"><button class="button is-hoverable is-dark" routerLink="/AboutMe">ABOUT ME</button></li>
-             </div>
+          <div class="column">
+            <li>
+              <<img
+                src="https://et710.com/website/war2/img/Visionaire.jpg"
+                class="image is-16by9"
+              /><button
+                class="button is-hoverable is-dark"
+                routerLink="/Paintings"
+              >
+                Paintings
+              </button>
+            </li>
+          </div>
 
-              <div class="column">
-              <li><img src="https://et710.com/website/war2/img/seudo-productions.jpg" class="image is-16by9"><button class="button is-hoverable is-dark" routerLink="/ContactMe">CONTACT ME</button></li>
-             </div>
+          <div class="column">
+            <li>
+              <img
+                src="https://et710.com/website/war2/img/name-logo.jpg"
+                class="image is-16by9"
+              /><button
+                class="button is-hoverable is-dark"
+                routerLink="/AboutMe"
+              >
+                ABOUT ME
+              </button>
+            </li>
+          </div>
 
-         </div>
- 
-        </ul>
+          <div class="column">
+            <li>
+              <img
+                src="https://et710.com/website/war2/img/seudo-productions.jpg"
+                class="image is-16by9"
+              /><button
+                class="button is-hoverable is-dark"
+                routerLink="/ContactMe"
+              >
+                CONTACT ME
+              </button>
+            </li>
+          </div>
+        </div>
+      </ul>
     </div>
-
-    
-      
-    
-   
-
   `,
-    styles: [ `
-    
-   `]
+  styles: [``]
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
